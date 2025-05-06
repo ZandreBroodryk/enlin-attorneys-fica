8f6180b03e25da3a2103369d388da190fde8a4f5
Zandré Broodryk <z.broodryk@gmail.com>
2025-05-06 15:56:29 +0200

    add icon to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,14 @@ const lexend = Lexend_Giga({
 export const metadata: Metadata = {
   title: "Enslin Inc. Attorneys - Client Fica Documents",
   description: "Your Trusted Partner In Law - Fica Required Forms ",
+  icons: {
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
